e7fa24a3ac4f718f6421a181012ffe9fe7d867c2
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-02-25 16:26:07 +0500

    Add files via upload

---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Personal_Message/main.js
@@ -0,0 +1,2 @@
+var personName = "Faiz";
+console.log("Hello ".concat(personName, ", would you like to learn some Pythob today?"));
---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Personal_Message/main.ts
@@ -0,0 +1,2 @@
+let personName: string = "Faiz";
+console.log(`Hello ${personName}, would you like to learn some Pythob today?`);
