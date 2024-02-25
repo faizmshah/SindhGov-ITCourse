c5a5afa15061a3d217d093cdb9152be91db9193a
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-02-25 20:46:49 +0500

    Add files via upload

---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Stripping_Names/main.js
@@ -0,0 +1,4 @@
+var personName = "\t    Faiz\tMuammad Shah      \n";
+console.log("Name with whitespace: \"".concat(personName, "\""));
+var strippedName = personName.trim();
+console.log("Stripped name: \"".concat(strippedName, "\""));
---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Stripping_Names/main.ts
@@ -0,0 +1,6 @@
+let personName: string = "\t    Faiz\tMuammad Shah      \n";
+
+console.log(`Name with whitespace: "${personName}"`);
+
+let strippedName: string = personName.trim();
+console.log(`Stripped name: "${strippedName}"`);
