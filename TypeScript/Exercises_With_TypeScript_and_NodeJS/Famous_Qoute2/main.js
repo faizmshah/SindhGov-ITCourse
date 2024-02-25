c40cd67e4d8d5eccec27767273d9903303d1aadb
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-02-25 20:44:33 +0500

    Add files via upload

---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Famous_Qoute2/main.js
@@ -0,0 +1,3 @@
+var famous_person = "William Shakespeare";
+var message = "".concat(famous_person, " once said, \"It is not in the stars to hold our destiny but in ourselves.\"");
+console.log(message);
---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Famous_Qoute2/main.ts
@@ -0,0 +1,4 @@
+let famous_person: string ="William Shakespeare";
+let message: string = `${famous_person} once said, "It is not in the stars to hold our destiny but in ourselves."`
+
+console.log(message);
