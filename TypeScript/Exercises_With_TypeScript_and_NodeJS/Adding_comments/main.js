02d219607b4d009b4122d82c432b4d450fd92527
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-02-27 17:21:22 +0500

    Add files via upload

---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Adding_comments/main.js
@@ -0,0 +1,10 @@
+// Famous Quote Program
+// By: Faiz Muhammad Shah
+// Date: 2024-02-22
+var famousQoute = "It is not in the stars to hold our destiny but in ourselves.";
+console.log("William Shakespear once said, \"".concat(famousQoute, "\""));
+// Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+console.log(4 + 4); // Addition
+console.log(15 - 7); // Subtraction
+console.log(2 * 4); // Multiplication
+console.log(16 / 2); // Division
