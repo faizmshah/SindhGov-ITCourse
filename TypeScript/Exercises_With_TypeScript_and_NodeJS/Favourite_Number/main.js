7ab29617835553c2707d237f079a4830105f1875
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-02-27 17:20:21 +0500

    Add files via upload

---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Favourite_Number/main.js
@@ -0,0 +1,3 @@
+var favouriteNumber = 5;
+var message = "My favorite number is ".concat(favouriteNumber, ".");
+console.log(message);
---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Favourite_Number/main.ts
@@ -0,0 +1,5 @@
+let favouriteNumber : number = 5;
+
+let message: string = `My favorite number is ${favouriteNumber}.`;
+
+console.log(message);
