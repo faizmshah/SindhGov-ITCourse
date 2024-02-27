26f5b02ec2920158b98780fd71d81df1aca30cf9
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-02-27 17:42:38 +0500

    Add files via upload

---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Greetings/main.js
@@ -0,0 +1,5 @@
+var names = ["Yasir", "Ali", "Tanveer", "Faiz", "Mussawar"];
+// prints personalied message to each person.
+names.forEach(function (name) {
+    console.log("Good morning, ".concat(name, "! Have a wonderful day!"));
+});
---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Greetings/main.ts
@@ -0,0 +1,6 @@
+let names: string[] = ["Yasir", "Ali", "Tanveer", "Faiz", "Mussawar"];
+
+// prints personalied message to each person.
+names.forEach((name) => {
+    console.log(`Good morning, ${name}! Have a wonderful day!`);
+});
