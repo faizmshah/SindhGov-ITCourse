55a7b0c3f1d91fc0a201d750184e282e94739e57
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-02-28 00:25:44 +0500

    Add files via upload

---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Your_Own_Array/main.js
@@ -0,0 +1,4 @@
+var favoriteTransport = ["Kia Sportage", "Honda City", "Toyota Fortuner", "Hyundai Santa Fe", "Toyota Land Cruiser"];
+favoriteTransport.forEach(function (vehicle) {
+    console.log("I would like to own a ".concat(vehicle, "."));
+});
---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Your_Own_Array/main.ts
@@ -0,0 +1,4 @@
+let favoriteTransport: string[] = ["Kia Sportage", "Honda City", "Toyota Fortuner", "Hyundai Santa Fe", "Toyota Land Cruiser"];
+
+favoriteTransport.forEach((vehicle) => {
+    console.log(`I would like to own a ${vehicle}.`);
