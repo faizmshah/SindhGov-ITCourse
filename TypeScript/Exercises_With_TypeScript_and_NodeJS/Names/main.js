9d9ed15ff07fc15813701436131ad3778bfa25dd
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-02-27 17:22:37 +0500

    Add files via upload

---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Names/main.js
@@ -0,0 +1,6 @@
+var names = ["Yasir", "Ali", "Tanveer", "Faiz", "Mussawar"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Names/main.ts
@@ -0,0 +1,7 @@
+let names: string[] = ["Yasir", "Ali", "Tanveer", "Faiz", "Mussawar"];
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
