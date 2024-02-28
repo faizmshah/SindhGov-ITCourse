7660c118def727dcb9782b8546d65dcf47922d03
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-02-28 17:09:57 +0500

    Add files via upload

---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Guest_List/main.js
@@ -0,0 +1,4 @@
+var friends = ["Ali", "Mustafa", "Tanveer", "Azeem", "Shafique"];
+friends.forEach(function (invitation) {
+    console.log("Hey ".concat(invitation, ", we're having a dinner at our place next Monday, would you like to come over?"));
+});
---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Guest_List/main.ts
@@ -0,0 +1,5 @@
+let friends: string[] = ["Ali", "Mustafa", "Tanveer", "Azeem", "Shafique"];
+
+friends.forEach((invitation) => {
+    console.log(`Hey ${invitation}, we're having a dinner at our place next Monday, would you like to come over?`);
+});
