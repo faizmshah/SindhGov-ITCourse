a181bbf93160098b96c6e27291a54df56e3824b6
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-03-19 21:43:15 +0500

    Add files via upload

---
+++ TypeScript/Exercises_With_TypeScript_and_NodeJS/Conditional_Tests/main.js
@@ -0,0 +1,27 @@
+// Task 23: Conditional Tests
+/* Write a series of conditional tests.
+Print a statement describing each test and your prediction for the results of each test.
+Your code should look something like this: */
+var car = "subaru";
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == "subaru");
+console.log("is car != 'SUBARU'? predict True.");
+console.log(car != 'SUBARU');
+var number = 5;
+console.log("Is number =='5'? I predict True.");
+console.log(number == 5);
+console.log("Is number != 10, I predict True");
+console.log(number != 10);
+console.log("Is number > 10? I predict False.");
+console.log(number > 10);
+console.log("Is number != 5, I predict False");
+console.log(number != 5);
+var fruits = ['apple', 'banana', 'orange'];
+console.log("Is fruits.length == 3, I predict True.");
+console.log(fruits.length == 3);
+console.log("is 9 > 87 ? predict false");
+console.log(9 > 87);
+console.log("is 3 <= 2 ? predict false");
+console.log(3 <= 2);
+console.log("is 72 >= 83 ? predict false");
+console.log(72 >= 83);
